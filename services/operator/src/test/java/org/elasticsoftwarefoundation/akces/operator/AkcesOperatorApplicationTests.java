02c6face14a905835896ce1f61fc073fa8e65e65
jwijgerd <jwijgerd@gmail.com>
2025-01-01 13:11:53 +0100

    fix native image building
    for now disable native image to test whether docker image can be pushed

--- services/operator/src/test/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplicationTests.java
+++ services/operator/src/test/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplicationTests.java
@@ -1,9 +1,11 @@
 package org.elasticsoftwarefoundation.akces.operator;
 
+import io.javaoperatorsdk.operator.springboot.starter.test.EnableMockOperator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableMockOperator
 class AkcesOperatorApplicationTests {
 
 	@Test
