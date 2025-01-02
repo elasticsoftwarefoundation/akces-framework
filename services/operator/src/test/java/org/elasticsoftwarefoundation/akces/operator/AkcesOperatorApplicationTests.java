f7b5c25a98604720d09bae20439b970f17e1a510
jwijgerd <jwijgerd@gmail.com>
2025-01-02 13:00:53 +0100

    enabled actuator health endpoint

--- services/operator/src/test/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplicationTests.java
+++ services/operator/src/test/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplicationTests.java
@@ -2,14 +2,31 @@ package org.elasticsoftwarefoundation.akces.operator;
 
 import io.javaoperatorsdk.operator.springboot.starter.test.EnableMockOperator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EnableMockOperator(crdPaths = {"classpath:META-INF/fabric8/aggregates.akces.elasticsoftwarefoundation.org-v1.yml"})
 class AkcesOperatorApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 
 	@Test
 	void contextLoads() {
+		assertThat(restTemplate).isNotNull();
+	}
+
+	@Test
+	void healthEndpointShouldBeEnabled() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/actuator/health",
+				String.class)).contains("{\"status\":\"UP\"}");
 	}
 
 }
