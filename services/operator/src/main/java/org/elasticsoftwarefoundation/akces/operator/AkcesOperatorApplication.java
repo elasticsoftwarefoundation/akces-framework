bc993e5fc159f16386744a56296813bcd1e1cf3d
jwijgerd <jwijgerd@gmail.com>
2025-01-03 12:21:20 +0100

    specifically add the BouncyCastleProvider
    set crypto.policy to unlimited

--- services/operator/src/main/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplication.java
+++ services/operator/src/main/java/org/elasticsoftwarefoundation/akces/operator/AkcesOperatorApplication.java
@@ -17,13 +17,18 @@
 
 package org.elasticsoftwarefoundation.akces.operator;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class AkcesOperatorApplication {
 
 	public static void main(String[] args) {
+		Security.setProperty("crypto.policy", "unlimited");
+		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(AkcesOperatorApplication.class, args);
 	}
 
