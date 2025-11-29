21182e3698f6b356b1fb44a49294e1d3acbe4728
jwijgerd <jwijgerd@gmail.com>
2025-11-29 17:03:37 +0100

    feat: add JdbcPostgresDialect bean and update Maven configuration for ClientConfig

--- test-apps/crypto-trading/queries/src/main/java/org/elasticsoftware/cryptotrading/ClientConfig.java
+++ test-apps/crypto-trading/queries/src/main/java/org/elasticsoftware/cryptotrading/ClientConfig.java
@@ -21,6 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -42,5 +43,16 @@ public class ClientConfig {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
                 .build();
+    }
+
+    /**
+     * see https://github.com/spring-projects/spring-boot/issues/48240
+     * see https://github.com/spring-projects/spring-boot/issues/47781
+     *
+     * @return the Postgres dialect
+     */
+    @Bean
+    JdbcPostgresDialect jdbcDialect() {
+        return JdbcPostgresDialect.INSTANCE;
     }
 }
